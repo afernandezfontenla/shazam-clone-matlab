53e40265326b22fa3516785e9919f452611041dc
afernandezfontenla <56158212+afernandezfontenla@users.noreply.github.com>
2026-03-30 18:06:48 +0200

    Delete CalcularEspectrogramaPrueba.m

--- CalcularEspectrogramaPrueba.m
+++
@@ -1,9 +0,0 @@
-[x,fs] = audioread('001.mp3');
-[S,F,T] = calcular_espectrograma(x,fs);
-
-imagesc(T,F,S);
-axis xy;
-xlabel('Tiempo (s)');
-ylabel('Frecuencia (Hz)');
-title('Espectrograma');
-colorbar;
